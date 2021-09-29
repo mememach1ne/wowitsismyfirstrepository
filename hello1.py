e294cb3f553022f262d05861c6a3c2a946325f57
Sergey <gorser0567@gmail.com>
2021-09-29 17:13:23 +0300

    Add hello1.py

---
+++ hello1.py
@@ -0,0 +1,13 @@
+def get_vat(payment, persent=20):
+	try:
+		payment = float(payment)
+		vat = payment / 100 * persent
+		vat=round(vat, 2)
+		return 'Sum VAT: {}'.format(vat)
+		return round(vat, 2)
+	except TypeError:
+		return "Can't count. Check data!"
+
+
+result = get_vat(400, 15)
+print(result)
